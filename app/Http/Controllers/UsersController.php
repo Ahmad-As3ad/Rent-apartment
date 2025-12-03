7ae53e03c3ee5f8f21145626a2ae559b19eb8741
kay-0770 <ahmad0000kay@gmail.com>
2025-12-03 21:53:07 +0300

    final test

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,18 +6,6 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-public function owaee(){
-    echo "hohgo";
-}
 
-
-public function owaesxx (){
-
-    echo "owaes am al jameh";//اويس عم الجميع
-}
-public function owasss(){
-echo "owaesss";
-
-}
 }
 
