c74671432fef536337dab99591e9a61c908ac799
kay-0770 <ahmad0000kay@gmail.com>
2025-12-03 12:37:29 +0300

    test

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,22 +6,5 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    function index()
-    {
-        $users = [
-            ['id' => 1, 'name' => "ahmad"],
-            ['id' => 2, 'name' => "khaled"],
-            ['id' => 3, 'name' => "mohammed"]
-        ];
-        // foreach ($users as $user)
-        // {
-        //     echo $user['id'] . '  , ' . $user['name'] . "\n";
-        // }
-     return response()->json($users);
     }
-    public function Checkuser(){
 
-
-
-    }
-}
