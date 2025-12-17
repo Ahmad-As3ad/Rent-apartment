387e1f809655f80d35621a48f32f13107e613d03
kay-0770 <ahmad0000kay@gmail.com>
2025-12-17 19:06:58 +0300

    ز

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,6 @@ Route::post('register', [AuthController::class, 'register']);
 
 //post هاد الراوت بيستخدم ال
 Route::post('login', [AuthController::class, 'login']);
-//بس لهون نحنا منحتاج
 
 
 
@@ -18,13 +17,15 @@ Route::post('login', [AuthController::class, 'login']);
 
 
 Route::middleware('auth:sanctum')->group(function () {
+// هاد يلي منأدخل من باقي المعلومات
+    Route::post('profile/update', [ProfileController::class, 'updateProfile']);
+
 
     Route::post('logout', [AuthController::class, 'logout']);
 
-    Route::post('profile/update', [ProfileController::class, 'updateProfile']);
 
     Route::middleware(['user.status', 'profile.complete'])->group(function () {
-
+//هاد بيرجع معلومات اليوزر بس اذا ماكنا مأدخلين المعلومات كاملة رح يطلع رسالة انو لازم نأدخل المعلومات
         Route::get('user', [AuthController::class, 'user']);
 
         Route::prefix(prefix: 'apartments')->group(function () {
